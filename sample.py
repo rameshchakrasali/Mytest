559095e63bcf652d685dd95812473409363c7531
rameshchakrasali <rameshchakrasali@kenscio.com>
2021-10-19 12:26:00 +0530

    Merged from different branches

--- sample.py
+++ sample.py
@@ -1,9 +1,5 @@
-
 I am in Master branch and making changes to this file
-
 print("Welcome to Kenscio")
 # I am in sample1 branch of Mytest original directory
-
-
 This is text generated in sample2 branch as third modification status
 
